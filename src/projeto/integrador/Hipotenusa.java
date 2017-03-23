9ec303b4fbcbccfc32f6dbcd1832b2887449ece6
ygor camargo <ygorcamargo55@gmail.com>
2017-03-23 20:20:59 -0300

    Solução 001

---
+++ src/projeto/integrador/Hipotenusa.java
@@ -0,0 +1,24 @@
+package projeto.integrador;
+
+import java.util.Scanner;
+
+public class Hipotenusa {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        double v1, v2, h,p;
+        
+        System.out.printf("entre com o valor 1:  ");
+        v1 = leitor.nextDouble();
+        
+        System.out.printf("entre com o valor 2:  ");
+        v2 = leitor.nextDouble();
+        
+        h = Math.sqrt(v1 * v1 + v2 * v2);
+        p = h + v1 + v2;
+        
+        System.out.println("A hipotenusa é :  :   " + h);
+        System.out.println("O perimetro é:  " + p);
+    }
+}
